8530f4b96af519f99a71b2290f430f016dc991cd
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-10-23 22:46:34 +0700

    chore: init project and few setup

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/colors/themes")["[data-theme=night]"],
+          primary: "#3ABFF8",
+          // "primary-focus": "#3b82f6",
+        },
+      },
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+};
---
+++ utils/interfaces.ts
@@ -0,0 +1,12 @@
+export interface User {
+  _id: string;
+  email: string;
+  name: string;
+  image?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+  session: string;
+  iat: number;
+  exp: number;
+}
